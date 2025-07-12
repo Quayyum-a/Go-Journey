d6b401b4d5e9ab7712f18621ad349374f766c090
Quayyum <quayyumariyo@gmail.com>
2025-07-12 17:59:12 +0100

    practice

--- src/golang-book/chapter6/slice.go
+++ src/golang-book/chapter6/slice.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
-func slice(){
-	slice1 := []int{1,2,3}
-slice2 := append(slice1, 4, 5)
+// func slice(){
+// 	slice1 := []int{1,2,3}
+// slice2 := append(slice1, 4, 5)
+// fmt.Println(slice1, slice2)
+// }
+
+func anotherSlice(){
+slice1 := []int{1,2,3}
+slice2 := make([]int, 2)
+copy(slice2, slice1)
 fmt.Println(slice1, slice2)
 }
 func main(){
-	slice()
+	// slice()
+	anotherSlice()
 }
