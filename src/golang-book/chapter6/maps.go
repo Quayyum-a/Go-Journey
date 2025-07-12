ae24201d1bf138bb2b7d36beef086f49b97b9c7a
Quayyum <quayyumariyo@gmail.com>
2025-07-12 18:11:41 +0100

    practice

--- src/golang-book/chapter6/maps.go
+++ src/golang-book/chapter6/maps.go
@@ -3,10 +3,25 @@ package main
 import "fmt"
 
 func maps(){
-	var x map[string]int
+x := make(map[string]int)
 x["key"] = 10
-fmt.Println(x)
+fmt.Println(x["key"])
+}
+func mapsFunction(){
+	elements := make(map[string]string)
+elements["H"] = "Hydrogen"
+elements["He"] = "Helium"
+elements["Li"] = "Lithium"
+elements["Be"] = "Beryllium"
+elements["B"] = "Boron"
+elements["C"] = "Carbon"
+elements["N"] = "Nitrogen"
+elements["O"] = "Oxygen"
+elements["F"] = "Fluorine"
+elements["Ne"] = "Neon"
+fmt.Println(elements["Li"])
 }
 func main(){
 	maps()
+	mapsFunction()
 }
